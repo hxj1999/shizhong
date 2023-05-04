c3bc4bb9523e045164c23d59a223c56bc00467ec
hxj1999 <63765441+hxj1999@users.noreply.github.com>
2023-05-04 18:13:12 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,16 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+# 在此处输入专注时间（以分钟为单位）
+focus_time = 60
+
+focus_timer(focus_time)
